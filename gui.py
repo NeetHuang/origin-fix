3c15ea2b9ed86fd1da4844f9d19c3fdd766902ba
NeetHuang <huang648474073@gmail.com>
2019-03-29 11:23:40 +0800

    fix BUG and auto runas
    2019-3-29 11:23:20

--- gui.py
+++ gui.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
-import _tkinter
 import tkinter
 import tkinter.messagebox as tkmessagebox#访问标准Tk对话框。
 import tkinter.filedialog as tkfiledialog#通用对话框，允许用户指定要打开或保存的文件。
-import shutil,re,os,winreg,base64,time
+import shutil,re,os,winreg,base64,time,sys
 from icon import img
+import win32com.shell.shell as shell
+ASADMIN = 'sudo'
+
 def  del_file(path):
 	for i in os.listdir(path):
 		path_file = os.path.join(path,i)
@@ -16,61 +18,35 @@ def  del_file(path):
 		else:
 			del_file(path_file)
 
-def fix_origin_A():
+def fix_origin_Full():
 	env_dist = os.environ
 	origin = env_dist.get('Appdata')  # 获取本地appdata路径
-	Roaming_origin1 = origin + '\\Origin'
-	local_origin2 = origin[0:-7] + 'Local\\Origin'
-	if os.path.exists(local_origin2):
-		os.system('taskkill /F /IM Origin.exe /T | taskkill /F /IM OriginWebHelperService.exe /T | taskkill /F /IM QtWebEngineProcess.exe /T | exit')
-		try:
-			shutil.rmtree(local_origin2) ####优先删除这个尝试#####
-		except:
-			del_file(local_origin2)
-		tkmessagebox.showinfo(title='成功',message='修复完成')
-	else:
-		tkmessagebox.showinfo(title='警告',message='目录未找到或已修复过')
-
-def fix_origin_B():
-	env_dist = os.environ
-	origin = env_dist.get('Appdata')  # 获取本地appdata路径
-	temp = env_dist.get('temp')
-	Roaming_origin1 = origin + '\\Origin'
-	local_origin2 = origin[0:-7] + 'Local\\Origin'
-	set_origin3 = "C:\\ProgramData\\Origin"
-	if os.path.exists(local_origin2):
-		os.system('taskkill /F /IM explorer.exe | taskkill /F /IM Origin.exe /T | taskkill /F /IM OriginWebHelperService.exe /T | taskkill /F /IM QtWebEngineProcess.exe /T | exit')
-		# os.system("tskill  Origin | tskill  OriginWebHelperService | tskill  QtWebEngineProcess | exit")
-		command_a = '''
-		attrib -r %APPDATA%\Origin /s
-		Takeown /F %APPDATA%\Origin /r /d y
-		cacls %APPDATA%\Origin /t /e /g Administrators:F
-		rd /s /q %APPDATA%\Origin
-		Takeown /F C:\ProgramData\Origin /r /d y
-		cacls C:\ProgramData\Origin /t /e /g Administrators:F
-		rd /s /q C:\ProgramData\Origin
-		Takeown /F %TEMP% /r /d y
-		cacls %TEMP% /t /e /g Administrators:F
-		rd /s /q %TEMP%
-		del /f /s /q %0
-		'''
-		try:
-			with open(temp+"/fixall.bat", 'w') as reg:
-					reg.write("@echo off\n"+command_a)
-					os.system(temp+"/fixall.bat")
-					time.sleep(5)
-					os.system('taskkill /F /IM cmd.exe | taskkill /F /IM conhost.exe | explorer.exe ')
-					tkmessagebox.showinfo(title='成功',message='最终修复完成,如果没有桌面请注销或重启')
-		except:
-			tkmessagebox.showinfo(title='警告',message='权限不足或BUG')
-	else:
-		tkmessagebox.showinfo(title='警告',message='目录未找到或已修复过')
+	Roaming_origin1 = origin + r'/Origin'
+	local_origin2 = origin[0:-7] + r'Local/Origin'
+	prodata3 = r'C:/ProgramData/Origin' 
+	# print(Roaming_origin1)
+	# print(local_origin2)
+	# print(prodata3)
+	os.system('taskkill /F /IM Origin.exe /T | taskkill /F /IM OriginWebHelperService.exe /T | taskkill /F /IM QtWebEngineProcess.exe /T | taskkill /F /IM explorer.exe /T')
+	try:
+		if os.path.isdir(Roaming_origin1):
+			shutil.rmtree(Roaming_origin1)
+		if os.path.isdir(local_origin2):
+			shutil.rmtree(local_origin2)
+		if os.path.isdir(prodata3):
+			shutil.rmtree(prodata3)
+	except:
+		del_file(Roaming_origin1)
+		del_file(local_origin2)
+		del_file(prodata3)
+	shell.ShellExecuteEx(lpVerb='runas', lpFile="C:/Windows/explorer.exe")
+	tkmessagebox.showinfo(title='成功',message='修复完成')
 
 def origin_akamai():
 	aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
 	get_key = winreg.OpenKey(aReg, r'SOFTWARE\\WOW6432Node\\Electronic Arts\\EA Core')  # 获取origin的注册表
 	origin_path = winreg.QueryValueEx(get_key, 'EADM6InstallDir')[0]  # 获取origin的注册表数据
-	EACore=origin_path+"\\EACore.ini"
+	EACore=origin_path+"/EACore.ini"#找到游戏目录里面的EACore.ini
 	text='''[connection]
 EnviromentName=production
 [Feature]
@@ -78,14 +54,20 @@ CdnOverride=akamai
 '''
 	with open(EACore, 'w') as EA:
 		EA.write(text)
-
-	hosts = ['184.28.218.98 origin-a.akamaihd.net', '23.198.105.69  www.origin.com']
+	backs =	shutil.copy(r'C:/Windows/System32/drivers/etc/hosts','C:/Windows/System32/drivers/etc/hosts.back')
+	hosts = ['23.57.66.80 origin-a.akamaihd.net', '69.192.10.215 www.origin.com','159.153.191.240 accounts.ea.com','159.153.191.238 gateway.ea.com']
 	if os.path.isfile('C:/Windows/System32/drivers/etc/hosts'):
 		with open(r'C:/Windows/System32/drivers/etc/hosts', 'r+') as hos:
 			if re.search(r"akamai",hos.read()):
-				tkmessagebox.showinfo(title='警告',message='已存在加速优化')
+				ask = tkmessagebox.askyesno(title='警告',message='已存在加速优化,是否仍要继续？')
+				if ask ==True:
+					backs
+					with open(r'C:/Windows/System32/drivers/etc/hosts', 'w') as hos:
+						for host in hosts:
+							hos.write("\n"+host+"\n")
+						tkmessagebox.showinfo(title='成功',message='加速优化完毕')
 			else:
-				shutil.copy(r'C:/Windows/System32/drivers/etc/hosts','C:/Windows/System32/drivers/etc/hosts.back')
+				backs
 				for host in hosts:
 				# print(host)
 					hos.write("\n"+host+"\n")
@@ -93,43 +75,26 @@ CdnOverride=akamai
 	else:
 		with open(r'C:/Windows/System32/drivers/etc/hosts', 'a') as hos:
 			for host in hosts:
-			# print(host)
 				hos.write("\n"+host+"\n")
 
 def file_path():
 	path_ = tkfiledialog.askdirectory(initialdir='C:/')
-	# print(path_)
 	tkinter.Label(text="你的安装位置-"+path_).place(x=35,y=170,anchor='w')
-	path = path_
-	game_path = '"'+path_+'/__Installer/Touchup.exe"'
-	check = path_+'/__Installer/Touchup.exe'
-	command_s = game_path+' install -locale {zh_TW} -installPath "{%s}" -autologging -startmenuIcon=1 -desktopIcon=1' %path
-	apex_path = path_ + '/__Installer/customcomponent/EasyAntiCheat/'
-	apex = apex_path + 'EasyAntiCheat_Setup.exe'
-	apex_command = '"' + apex + '" install 154 -console'
-	# print(apex)
-	# print(apex_command)
-	if os.path.isfile(check):
-			env_dist = os.environ
-			temp = env_dist.get('temp')
-			try:
-				if os.path.isfile(apex):
-					# print("APEX")
-					with open(temp+'/regapex.bat', 'w') as reg:
-						reg.write(apex_command)
-					os.system(temp+'\\regapex.bat')
-					with open(temp+'/regfix.bat', 'w') as reg:
-						reg.write(command_s)
-					os.system(temp+'\\regfix.bat')
-				else:
-					# print("NO-APEX")
-					with open(temp+'/regfix.bat', 'w') as reg:
-						reg.write(command_s)
-					os.system(temp+'\\regfix.bat')
-				time.sleep(2)
-				tkmessagebox.showinfo(title='成功',message='修复完成')
-			except:
-				tkmessagebox.showinfo(title='失败',message='请使用管理员权限打开或者检查文件')
+	Game_Path = path_+r'/__Installer/Touchup.exe'
+	Game_Fix = r'install -locale zh_TW -installPath "%s" -autologging -startmenuIcon=1 -desktopIcon=1' %path_
+	EasyAnti_Path = path_ + r'/__Installer/customcomponent/EasyAntiCheat/EasyAntiCheat_Setup.exe'
+	EasyAnti_Fix = r'install 154 -console'
+	if os.path.isfile(Game_Path):
+		tkmessagebox.showinfo(title='提示',message='修复中,请等待数十秒')
+		if os.path.isfile(EasyAnti_Path):
+			#print("APEX")
+			shell.ShellExecuteEx(lpVerb='runas', lpFile=EasyAnti_Path, lpParameters=EasyAnti_Fix)
+			shell.ShellExecuteEx(lpVerb='runas', lpFile=Game_Path, lpParameters=Game_Fix)
+		else:
+			#print("NO-EasyAnti")
+			shell.ShellExecuteEx(lpVerb='runas', lpFile=Game_Path, lpParameters=Game_Fix)
+		time.sleep(10)
+		tkmessagebox.showinfo(title='成功',message='修复完成,请检查桌面是否出现游戏图标,如果提示VC安装失败,请进入游戏目录__Installer/vc,手动安装成功后再次运行')
 	else:
 		tkmessagebox.showinfo(title='提示',message='请检查是否为游戏目录且存在__Installer目录')
 
@@ -138,35 +103,27 @@ def main():
 	fm1 = tkinter.Frame()
 	label = tkinter.Label(text='origin多功能修复工具',height=2,width=20)
 	label.pack(side='top')
-	fm1.pack()
+	#fm1.pack()
 
-	fm2 = tkinter.Frame()
+	#fm2 = tkinter.Frame()
 	LabelA=tkinter.Label(text='修复origin发生了一些问题')
-	A = tkinter.Button(text ="修复", command = fix_origin_A,height=1,width = 10)
-	LabelA.place(x=35,y=60,anchor='w')
+	A = tkinter.Button(text ="修复", command = fix_origin_Full,height=1,width = 10)
+	LabelA.place(x=20,y=60,anchor='w')
 	A.place(x=210,y=60,anchor='w')
 
-	LabelB=tkinter.Label(text='最终修复方案')
-	B = tkinter.Button(text ="修复", command = fix_origin_B,height=1,width = 10)
-	LabelB.place(x=35,y=100,anchor='w')
+	LabelB =tkinter.Label(text='origin加速下载和解决登陆问题')
+	B = tkinter.Button(text ="加速", command = origin_akamai,height=1,width = 10)
+	LabelB.place(x=20,y=100,anchor='w')
 	B.place(x=210,y=100,anchor='w')
 
-	LabelC =tkinter.Label(text='origin加速')
-	C = tkinter.Button(text ="加速", command = origin_akamai,height=1,width = 10)
-	LabelC.place(x=35,y=140,anchor='w')
-	C.place(x=210,y=140,anchor='w')
+	LabelC=tkinter.Label(text='游戏位置移动(免验证完整性)')
+	C = tkinter.Button(text ="选择游戏目录", command = file_path,height=1,width = 15)
+	LabelC.place(x=20,y=140,anchor='w')
+	C.place(x=190,y=140,anchor='w')
+	fm1.pack()
 
-	LabelD=tkinter.Label(text='游戏位置修改后修复')
-	D = tkinter.Button(text ="选择游戏目录", command = file_path,height=1,width = 15)
-	LabelD.place(x=35,y=200,anchor='w')
-	D.place(x=190,y=200,anchor='w')
-
-	tkmessagebox.showinfo(title='注意',message='请使用管理员权限打开,已使用管理员打开请无视')
-	fm2.pack()
-
-
-	root.title('origin多功能修复工具V2.1')#标题
-	root.geometry('320x220')#窗体大小
+	root.title('origin多功能修复工具V3.0')#标题
+	root.geometry('320x200')#窗体大小
 	root.resizable(False, False)#固定窗体
 	tmp = open("tmp.ico","wb+")
 	tmp.write(base64.b64decode(img))
@@ -176,4 +133,11 @@ def main():
 	root.mainloop()
 
 if __name__ == '__main__':
-	main()
+	#获取管理员权限
+	if sys.argv[-1] != ASADMIN:
+	    script = os.path.abspath(sys.argv[0])
+	    params = ' '.join([script] + sys.argv[1:] + [ASADMIN])
+	    shell.ShellExecuteEx(lpVerb='runas', lpFile=sys.executable, lpParameters=params)
+	    sys.exit(0)
+	else:
+		main()
